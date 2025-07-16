79de828fd2a8520eb5f43d41eec4c90ec0a73baa
nishb <nishbcodes@gmail.com>
2025-07-16 14:20:50 +0530

    Route and Sidebar UI Added

---
+++ src/app/dashboard/operation/page.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Operation Management",
+};
+
+export default function Page() {
+  return (
+    <div>
+      <div>Operation DB</div>
+    </div>
+  );
+}
