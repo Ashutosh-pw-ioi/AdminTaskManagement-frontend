c3a45124b131e99904c84a885f8618ea7dbaa5d3
nishb <nishbcodes@gmail.com>
2025-07-16 19:58:52 +0530

    Admin and Operation Auth Added

---
+++ src/app/auth/login/operation/page.tsx
@@ -0,0 +1,9 @@
+import LoginPage from "../../loginPage";
+
+export const metadata = {
+  title: "Operation Login",
+};
+
+export default function OperationLoginPage() {
+  return <LoginPage role="operation" imagePath="/OperationLogin.png" />;
+}
